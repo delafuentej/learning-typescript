cc9b30dd10dd8408f83f7a1e7bd0d869ba7b45f1
Jesus de la Fuente <jfuentelucas@gmail.com>
2021-12-14 18:03:55 +0100

    custom types

--- complexTypes/index.js
+++ complexTypes/index.js
@@ -193,3 +193,250 @@ let dinner = ["Chicken", "Bristek", "Spaghetti"];
 let lunch = ["Sandwich", "Soup", "Bread"];
 let todaysMenu = [["Cereal", "Toast"], dinner];
 let weeklyMenu = [todaysMenu, [lunch]];
+console.log("---CUSTOM TYPES---");
+//Custom Types enable type checking that is tailores to your exact purposes: tuples 
+//let myVar:comType;
+//const testFn =(param:comType):returnedCompType =>{
+//function body
+// }
+console.log("---Custom Types: Enums---");
+// To enumerate all the possible values that a variable could have
+/*
+enum DirectionNumber{
+
+    North,
+    South,
+    East,
+    West
+}
+let wichWayToArticOcean:Direction;
+wichWayToArticOcean:Direction.North;
+wichWayToArticOcean:Direction.Southeast;//Property 'Southeast' does not exist on type 'typeof Direction'.ts(2339)
+
+console.log(Direction.North===0);//true
+console.log(Direction.South===2);//This condition will always return 'false' since the types 'Direction.South' and '2' have no overlap.ts(2367)
+*/
+let petOnSale = "chinchilla";
+//list of orders to process:ordersArray [pet name string, number ordered]
+let ordersArray = [
+    ["rat", 2],
+    ["chinchilla", 1],
+    ["hamster", 2],
+    ["tarantula", 5]
+];
+var Pet;
+(function (Pet) {
+    Pet[Pet["Hamster"] = 0] = "Hamster";
+    Pet[Pet["Rat"] = 1] = "Rat";
+    Pet[Pet["Chinchilla"] = 2] = "Chinchilla";
+    Pet[Pet["Tarantula"] = 3] = "Tarantula";
+})(Pet || (Pet = {}));
+const petOnSaleTS = Pet.Chinchilla;
+/*
+let ordersArrayTS= [Pet, number][]=[
+    
+    [Pet.Rat,2],
+    [Pet.Chinchilla,1],
+    [Pet.Hamster, 2],
+    [Pet.Tarantula,5]
+
+] */
+//ordersArrayTS.push([Pet.Jerboa,3])//Property 'Jerboa' does not exist on type 'typeof Pet'.ts(2339)
+console.log("---String Enums VS Numeric Enums---");
+//TS allows us to use enums based on strings referred to as string enums
+//We must write the enums string explicitly
+//Convention: the string value of the enum variable is just the capitalized form of the variable name
+var DirectionString;
+(function (DirectionString) {
+    DirectionString["North"] = "NORTH";
+    DirectionString["South"] = "SOUTH";
+    DirectionString["East"] = "EAST";
+    DirectionString["West"] = "WEST";
+})(DirectionString || (DirectionString = {}));
+var Pet2;
+(function (Pet2) {
+    Pet2["Hamster"] = "HAMSTER";
+    Pet2["Rat"] = "RAT";
+    Pet2["Chinchilla"] = "CHINCHILLA";
+    Pet2["Tarantula"] = "TARANTULA";
+})(Pet2 || (Pet2 = {}));
+const petOnSaleTS2 = Pet2.Chinchilla;
+let ordersArrayTS2 = [
+    [Pet2.Rat, 2],
+    [Pet2.Chinchilla, 1],
+    [Pet2.Hamster, 3],
+    [Pet2.Chinchilla, 10]
+];
+ordersArrayTS2.push(["HAMSTER", 6]); //Type '"HAMSTER"' is not assignable to type 'Pet2'.ts(2322)
+ordersArrayTS2.push([Pet2.Tarantula, 10]);
+console.log(ordersArrayTS2);
+/*
+[
+  [ 'RAT', 2 ],
+  [ 'CHINCHILLA', 1 ],
+  [ 'HAMSTER', 3 ],
+  [ 'CHINCHILLA', 10 ],
+  [ 'HAMSTER', 6 ],
+  [ 'TARANTULA', 10 ]
+]
+*/
+console.log("---OBJECT TYPES--");
+let aPerson;
+/* aPerson={
+    name:"Alison",
+    age:"20"//Type 'string' is not assignable to type 'number'.ts(2322)
+
+}
+ */
+/* aPerson={
+    name:"Anne",
+    yearsOld:20 //Type '{ name: string; yearsOld: number; }' is not assignable to type '{ name: string; age: number; }'.
+
+} */
+aPerson = {
+    name: "Jon",
+    age: 25
+};
+let aCompany;
+console.log("---------------------------------");
+const sayHappyBirthdayWithObject = (personObject) => {
+    let output = '';
+    output += 'Happy Birthday '
+        + personObject.name + '! ';
+    output += 'You are now '
+        + personObject.age + ' years old! ';
+    output += 'Your birthday wish was to receive '
+        + personObject.giftWish
+        + '. And guess what? You will ';
+    if (!personObject.success) {
+        output += 'not ';
+    }
+    output += 'receive it! \n';
+    console.log(output);
+};
+let birthdayBabies = [
+    { name: 'Liam', age: 30, giftWish: 'karate skills', success: false },
+    { name: 'Olivia', age: 40, giftWish: 'a bright future', success: true },
+    { name: 'Ava', age: 35, giftWish: '$0.25', success: true }
+];
+birthdayBabies.forEach(sayHappyBirthdayWithObject);
+/*
+Happy Birthday Liam! You are now 30 years old! Your birthday wish was to receive karate skills. And guess what? You will not receive it!
+
+Happy Birthday Olivia! You are now 40 years old! Your birthday wish was to receive a bright future. And guess what? You will receive it!
+
+Happy Birthday Ava! You are now 35 years old! Your birthday wish was to receive $0.25. And guess what? You will receive it!
+
+*/
+console.log("-----------------------------");
+console.log("---Type Aliases---");
+let aCompany2;
+let caCoordinates = [40, 50, "N", -60.9, 49, "W"];
+console.log("---FUNCTION TYPES---");
+let myFunc;
+myFunc = (firstName, lastName) => {
+    return firstName.length + lastName.length;
+};
+const median = (items) => {
+    items.sort();
+    return items[Math.floor(items.length / 2)];
+};
+console.log(median([5, 9, 1, 3]));
+console.log(myFunc("Jesus", "de la Fuente")); //17
+// Math Operations
+function add(a, b) { return a + b; }
+function subtract(a, b) { return a - b; }
+function multiply(a, b) { return a * b; }
+function divide(a, b) { return a / b; }
+function wrongAdd(a, b) { return (a + b) + ''; }
+// Math Tutor Function That Accepts a Callback
+function mathTutor(operationCallback) {
+    console.log("Let's learn how to", operationCallback.name, '!');
+    let value25 = operationCallback(2, 5);
+    console.log('When we', operationCallback.name, '2 and 5, we get', value25, '.');
+    console.log('When we', operationCallback.name, value25, 'and 7, we get', operationCallback(value25, 7), '.');
+    console.log('Now fill out this worksheet.');
+}
+// Call your functions below:
+mathTutor(add);
+mathTutor(wrongAdd);
+console.log("---Generic Types---");
+let stringFamily = {
+    parents: ["Jan", "Susane"],
+    mate: "string",
+    children: ["Potato", "Patata", "Potatín"]
+};
+console.log("---------------------");
+//Do not change the code above this line.
+//Provide type annotations for the variables below:
+let theFamily = {
+    parents: [3, 4], mate: 9, children: [5, 30, 121]
+};
+let someFamily = {
+    parents: [true, true], mate: false,
+    children: [false, false, true, true]
+};
+let aFamily = {
+    parents: [
+        { name: 'Mom', job: 'software engineer' },
+        { name: 'Dad', job: 'coding engineer' }
+    ],
+    mate: { name: 'Matesky', job: 'engineering coder' },
+    children: [{ name: 'Babesky', job: 'none' }]
+};
+let anotherFamily = {
+    parents: [
+        { name: 'Momo', tailWagSpeed: 3 },
+        { name: 'Dado', tailWagSpeed: 100 }
+    ],
+    mate: { name: 'Cheems', tailWagSpeed: 7 },
+    children: [
+        { name: 'Puppin', tailWagSpeed: 0.001 },
+        { name: 'Puppenaut', tailWagSpeed: 0.0001 },
+        { name: 'Puppenator', tailWagSpeed: 0.01 }
+    ]
+};
+console.log("---Generic Functions---");
+const getFilledArray = (value, n) => {
+    return Array(n).fill(value);
+};
+console.log(getFilledArray(5, 3)); //[ 5, 5, 5 ]
+console.log(getFilledArray("potatoes", 4)); //[ 'potatoes', 'potatoes', 'potatoes', 'potatoes' ]
+const getFilledArray2 = (value, n) => {
+    return Array(n).fill(value);
+};
+console.log(getFilledArray2(true, 8));
+/*
+[
+  true, true, true,
+  true, true, true,
+  true, true
+]
+*/
+function getFilledArray3(value, n) {
+    return Array(n).fill(value);
+}
+let stringArray;
+let numberArray;
+let personArray;
+let coordinateArray;
+// Write your code below:
+stringArray = getFilledArray3("morning", 3);
+console.log(stringArray); //[ 'morning', 'morning', 'morning' ]
+numberArray = getFilledArray3(5, 2);
+console.log(numberArray);
+/* [ 5, 5 ] */
+personArray = getFilledArray3({
+    name: "Mr.Potato",
+    age: 89
+}, 3);
+console.log(personArray);
+/* [
+{ name: 'Mr.Potato', age: 89 },
+{ name: 'Mr.Potato', age: 89 },
+{ name: 'Mr.Potato', age: 89 }
+] */
+coordinateArray = getFilledArray3([-5.6, 8], 2);
+console.log(coordinateArray);
+/*
+[ [ -5.6, 8 ], [ -5.6, 8 ] ] */
--- complexTypes/index.ts
+++ complexTypes/index.ts
@@ -277,6 +277,395 @@ let weeklyMenu:string[][][]=[todaysMenu,[lunch]]
 console.log("---CUSTOM TYPES---")
 
 
-//Custom Types enable type checking that is tailores to your exact purposes: tuples
+//Custom Types enable type checking that is tailores to your exact purposes: tuples 
+//let myVar:comType;
+//const testFn =(param:comType):returnedCompType =>{
+        //function body
+// }
+console.log("---Custom Types: Enums---")
 
+// To enumerate all the possible values that a variable could have
+/*
+enum DirectionNumber{
+
+    North,
+    South,
+    East,
+    West
+}
+let wichWayToArticOcean:Direction;
+wichWayToArticOcean:Direction.North;
+wichWayToArticOcean:Direction.Southeast;//Property 'Southeast' does not exist on type 'typeof Direction'.ts(2339)
+
+console.log(Direction.North===0);//true
+console.log(Direction.South===2);//This condition will always return 'false' since the types 'Direction.South' and '2' have no overlap.ts(2367)
+*/
+
+let petOnSale="chinchilla";
+//list of orders to process:ordersArray [pet name string, number ordered]
+let ordersArray=[
+    ["rat",2],
+    ["chinchilla",1],
+    ["hamster",2],
+    ["tarantula",5]
+
+];
+
+enum Pet{
+
+    Hamster,
+    Rat,
+    Chinchilla,
+    Tarantula
+}
+const petOnSaleTS: Pet =Pet.Chinchilla;
+/* 
+let ordersArrayTS= [Pet, number][]=[
+    
+    [Pet.Rat,2],
+    [Pet.Chinchilla,1],
+    [Pet.Hamster, 2],
+    [Pet.Tarantula,5]
+
+] */
+
+//ordersArrayTS.push([Pet.Jerboa,3])//Property 'Jerboa' does not exist on type 'typeof Pet'.ts(2339)
+
+console.log("---String Enums VS Numeric Enums---")
+
+//TS allows us to use enums based on strings referred to as string enums
+//We must write the enums string explicitly
+//Convention: the string value of the enum variable is just the capitalized form of the variable name
+
+enum DirectionString{
+
+    North="NORTH",
+    South="SOUTH",
+    East="EAST",
+    West="WEST"
+}
+
+enum Pet2{
+    Hamster="HAMSTER",
+    Rat="RAT",
+    Chinchilla="CHINCHILLA",
+    Tarantula="TARANTULA"
+
+}
+const petOnSaleTS2:Pet2=Pet2.Chinchilla;
+
+let ordersArrayTS2:[Pet2,number][]=[
+    [Pet2.Rat,2],
+    [Pet2.Chinchilla,1],
+    [Pet2.Hamster,3],
+    [Pet2.Chinchilla,10]
+
+]
+ordersArrayTS2.push(["HAMSTER",6]);//Type '"HAMSTER"' is not assignable to type 'Pet2'.ts(2322)
+
+ordersArrayTS2.push([Pet2.Tarantula,10])
+
+console.log(ordersArrayTS2)
+/* 
+[
+  [ 'RAT', 2 ],
+  [ 'CHINCHILLA', 1 ],
+  [ 'HAMSTER', 3 ],
+  [ 'CHINCHILLA', 10 ],
+  [ 'HAMSTER', 6 ],
+  [ 'TARANTULA', 10 ]
+]
+*/
+
+console.log("---OBJECT TYPES--")
+
+
+
+let aPerson:{
+    name:string,
+    age:number
+}
+
+/* aPerson={
+    name:"Alison",
+    age:"20"//Type 'string' is not assignable to type 'number'.ts(2322)
+
+}
+ */
+
+/* aPerson={
+    name:"Anne",
+    yearsOld:20 //Type '{ name: string; yearsOld: number; }' is not assignable to type '{ name: string; age: number; }'.
+
+} */
+aPerson={
+    name:"Jon",
+    age:25
+}
+
+let aCompany:{
+    companyName:string,
+    boss:{
+        name:string,
+        age:number
+    },
+    employees:{
+        name:string,
+        age:number
+
+    },
+    employessOfTheMonth:{
+        name:string,
+        age:number
+    },
+    moneyEarned:number
+
+}
+
+
+
+console.log("---------------------------------")
+
+const sayHappyBirthdayWithObject=(personObject:{ name:string, age:number, giftWish:string, success:boolean
+})=>{
+  let output ='';
+  output += 'Happy Birthday '
+         + personObject.name + '! ';
+  output += 'You are now ' 
+         + personObject.age + ' years old! ';
+  output += 'Your birthday wish was to receive ' 
+         + personObject.giftWish 
+         + '. And guess what? You will ';
+  if (!personObject.success){
+    output += 'not ';
+  }
+  output += 'receive it! \n';
+  console.log(output);
+}
+
+
+let birthdayBabies: {name: string, age: number, giftWish: string, success: boolean}[] = [
+  {name: 'Liam', age: 30, giftWish: 'karate skills', success: false}, 
+  {name: 'Olivia', age: 40, giftWish: 'a bright future', success:true}, 
+  {name: 'Ava', age: 35, giftWish: '$0.25', success:true}
+]; 
+
+birthdayBabies.forEach(sayHappyBirthdayWithObject);
+/* 
+Happy Birthday Liam! You are now 30 years old! Your birthday wish was to receive karate skills. And guess what? You will not receive it! 
+
+Happy Birthday Olivia! You are now 40 years old! Your birthday wish was to receive a bright future. And guess what? You will receive it! 
+
+Happy Birthday Ava! You are now 35 years old! Your birthday wish was to receive $0.25. And guess what? You will receive it! 
+
+*/
+
+console.log("-----------------------------")
+
+console.log("---Type Aliases---")
+
+
+/* let aCompany:{
+    companyName:string,
+    boss:{
+        name:string,
+        age:number
+    },
+    employees:{
+        name:string,
+        age:number
+
+    },
+    employessOfTheMonth:{
+        name:string,
+        age:number
+    },
+    moneyEarned:number
+
+} */
+type Person={
+    name:string,
+    age:number
+}
+
+let aCompany2:{
+    companyName:string,
+    boss:Person,
+    employees:Person[],
+    employessOfTheMonth:Person,
+    moneyEarned:number
+
+}
+
+type Coord=[
+    number,
+    number,
+    string,
+    number,
+    number,
+    string
+]
+
+let caCoordinates:Coord =[40,50,"N",-60.9,49,"W"];
+
+console.log("---FUNCTION TYPES---")
+
+let myFunc:StringsToNumberFunction;
+
+myFunc=(firstName:string,lastName:string)=>{
+
+    return firstName.length+ lastName.length;
+}
+
+
+const median=<Item> (items:Array<Item>)=>{
+    items.sort();
+    return items[Math.floor(items.length/2)];
+
+}
+
+console.log(median<number>([5,9,1,3]))
+
+console.log(myFunc("Jesus","de la Fuente");//17
+
+
+
+// Math Operations
+function add(a, b){return a+b }
+function subtract(a, b){return a-b }
+function multiply(a, b){return a*b}
+function divide(a, b){return a/b}
+function wrongAdd(a, b){return (a+b)+''}
+
+// Add your function type below:
+type OperatorFunction= (a:number, b:number)=>number;
+
+
+// Math Tutor Function That Accepts a Callback
+function mathTutor(operationCallback: OperatorFunction) {
+  console.log("Let's learn how to", operationCallback.name,'!');
+  let value25 = operationCallback(2,5);
+  console.log('When we', operationCallback.name, '2 and 5, we get', value25, '.');
+  console.log('When we', operationCallback.name, value25, 'and 7, we get', operationCallback(value25,7), '.');
+  console.log('Now fill out this worksheet.');
+}
+
+// Call your functions below:
+mathTutor(add)
+mathTutor(wrongAdd)
+
+console.log("---Generic Types---")
+
+type Family<T>={
+    parents:[T,T],
+    mate:T,
+    children:T[]
+
+}
+let stringFamily: Family<string>={
+
+    parents:["Jan","Susane"],
+    mate:"string",
+    children:["Potato","Patata","Potatín"]
+}
+
+console.log("---------------------")
+
+type Human = {name: string, job: string};
+type Dog = {name: string, tailWagSpeed: number};
+
+type Family<T> = {
+  parents: [T, T], mate: T, children: T[]
+};
+//Do not change the code above this line.
+
+//Provide type annotations for the variables below:
+let theFamily: Family<number> = {
+  parents: [3, 4], mate: 9, children: [5, 30, 121]
+};
+
+let someFamily: Family<boolean> = {
+  parents: [true, true], mate: false, 
+  children: [false, false, true, true]
+};
+
+let aFamily: Family<Human> = {
+  parents: [
+    {name: 'Mom', job: 'software engineer'},
+    {name: 'Dad', job: 'coding engineer'}
+  ],
+  mate: {name: 'Matesky', job: 'engineering coder'},
+  children: [{name: 'Babesky', job: 'none'}]
+};
+
+let anotherFamily : Family<Dog>= {
+  parents: [
+    {name: 'Momo', tailWagSpeed: 3},
+    {name: 'Dado', tailWagSpeed: 100}
+  ],
+  mate: {name: 'Cheems', tailWagSpeed: 7},
+  children: [
+    {name: 'Puppin', tailWagSpeed: 0.001},
+    {name: 'Puppenaut', tailWagSpeed: 0.0001},
+    {name: 'Puppenator', tailWagSpeed: 0.01}
+  ]
+};
+
+console.log("---Generic Functions---")
+
+
+const getFilledArray=(value,n)=>{
+    return Array(n).fill(value)
+}
+
+console.log(getFilledArray(5,3));//[ 5, 5, 5 ]
+console.log(getFilledArray("potatoes",4));//[ 'potatoes', 'potatoes', 'potatoes', 'potatoes' ]
+
+
+const getFilledArray2=<T>(value:T, n:number):T[]{
+    return Array(n).fill(value)
+}
+console.log(getFilledArray2(true,8))
+
+/* 
+[
+  true, true, true,
+  true, true, true,
+  true, true
+]
+*/
+
+function getFilledArray3<T>(value: T, n: number): T[] {
+    return Array(n).fill(value);
+  }
+  
+  let stringArray: string[];
+  let numberArray: number[];
+  let personArray: {name: string, age: number}[];
+  let coordinateArray: [number, number][];
+  
+  // Write your code below:
+  stringArray=getFilledArray3<string>("morning",3);
+  console.log(stringArray);//[ 'morning', 'morning', 'morning' ]
+  numberArray=getFilledArray3<number>(5,2);
+  console.log(numberArray);
+  /* [ 5, 5 ] */
+  personArray=getFilledArray3<{name:string, age:number}>({
+
+    name:"Mr.Potato",
+    age:89
+  },3
+   
+  )
+  console.log(personArray);
+  /* [
+  { name: 'Mr.Potato', age: 89 },
+  { name: 'Mr.Potato', age: 89 },
+  { name: 'Mr.Potato', age: 89 }
+] */
+
+coordinateArray=getFilledArray3<[number,number]>([-5.6,8],2)
+console.log(coordinateArray)
+/* 
+[ [ -5.6, 8 ], [ -5.6, 8 ] ] */
 
